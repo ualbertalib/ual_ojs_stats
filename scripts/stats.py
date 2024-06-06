6747f51bf1a1512a55b639c0cb2331145219297b
henry zhang <henry@hzlinux.library.ualberta.ca>
2024-06-06 00:25:39 -0600

    output format

--- scripts/stats.py
+++ scripts/stats.py
@@ -10,23 +10,18 @@ import datetime
 def get_previous_month():
    
    today = datetime.date.today()
-   
    first = today.replace(day=1)
-   
    previous_month = first - datetime.timedelta(days=1)
-   
    month_lookup = previous_month.strftime("%Y-%m")
-   
    return month_lookup
 
 if __name__ == '__main__':
 
    fname=sys.argv[1]
-
    journals = pd.read_csv(fname) 
-
    previous_month=get_previous_month()
 
+   stats=[]
 
    for ind in journals.index:
      jabbr=journals["jabbr"][ind]
@@ -39,7 +34,17 @@ if __name__ == '__main__':
      abviews=jnl.get_abviews(f"{previous_month}-01")
      galley=jnl.get_galley_views(f"{previous_month}-01")
      pubs=jnl.get_publications()
-     print(subs["itemsMax"])
-     print(abviews)
-     print(galley)
-     print(pubs)
+
+     stat=dict()
+
+     stat={
+        "journal abbreviation": jabbr,
+        "published submissions": subs["itemsMax"],
+        "published issues": issues["itemsMax"],
+        "abstract views": abviews[0]["value"],
+        "galley views": galley[0]["value"]
+     }
+     stats.append(stat)
+
+   strs = json.dumps(stats, indent=4)
+   print(strs)
