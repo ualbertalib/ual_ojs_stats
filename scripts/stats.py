395b7b1031014a3ba1b6f1aff7dc2fd47c3c7772
henryzhang87 <3227948+henryzhang87@users.noreply.github.com>
2024-06-06 00:23:22 -0600

    Update stats.py

--- scripts/stats.py
+++ scripts/stats.py
@@ -7,26 +7,18 @@ from ojs import *
 from journal import *
 import datetime
 
-def get_previous_month():
-   
+def get_previous_month():   
    today = datetime.date.today()
-   
    first = today.replace(day=1)
-   
    previous_month = first - datetime.timedelta(days=1)
-   
    month_lookup = previous_month.strftime("%Y-%m")
-   
    return month_lookup
 
 if __name__ == '__main__':
-
+   
    fname=sys.argv[1]
-
    journals = pd.read_csv(fname) 
-
    previous_month=get_previous_month()
-
 
    for ind in journals.index:
      jabbr=journals["jabbr"][ind]
