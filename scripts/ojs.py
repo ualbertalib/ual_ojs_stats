9aecaedabbf12f0da1eb00f4c0fd35cf9474039b
henryzhang87 <3227948+henryzhang87@users.noreply.github.com>
2024-06-13 10:27:15 -0600

    Update ojs.py

--- scripts/ojs.py
+++ scripts/ojs.py
@@ -1,13 +1,28 @@
 class OJS:
+  """
+  OJS is a base class that represents an OJS host. It has a base URL that
+  represents the host. For example, https://journals.library.ualberta.ca/
+
+  ...
+
+  Attributes
+  -----------------------------------------------------------------------
+  base_url: str
+      the host url, i.e., https://journals.library.ualberta.ca/
+  
+  """
   def __init__(self,base_url):
      self._base_url=base_url
   
   @property
   def base_url(self):
+     # get base_url
      return self._base_url
+    
 
   @base_url.setter
   def base_url(self,value):
+    # set base_url
     self._base_url=value
 
  
