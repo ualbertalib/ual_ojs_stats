2c82a7b221d548660d027953879c4520aeebf862
henry zhang <henry@hzlinux.library.ualberta.ca>
2025-06-25 07:37:44 -0600

    refactoring the code

--- scripts/chart.py
+++ scripts/chart.py
@@ -1,5 +1,7 @@
 from openpyxl import Workbook
 from openpyxl import load_workbook
+from openpyxl.styles import numbers
+
 import datetime
 
 class Chart: 
@@ -21,20 +23,6 @@ class Chart:
   def get_worksheet(self, name):
     return self._wb[name] 
 
-
-  def update_monthly_views(self,stats,coverage_month,report_date,col_names):
-    ws_report=self.get_worksheet("Report")
-    ws_report["I1"].value=str(coverage_month)
-    ws_report["C4"].value=str(report_date)
-
-    ws_stat=self.get_worksheet("Statistics")
-    row=3
-    for stat in stats:
-      cols=[ws_stat[f"A{row}"],ws_stat[f"B{row}"],ws_stat[f"C{row}"],ws_stat[f"D{row}"],ws_stat[f"E{row}"]]
-      row=row+1
-      for j in range(0,len(cols)):
-         cols[j].value=stat[col_names[j]]
-  
 
   def save_workbook(self):
     self._wb.save(self._chart_file)
